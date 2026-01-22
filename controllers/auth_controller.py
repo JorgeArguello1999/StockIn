638db8e160baeaac3128c2d194b4198baa82b630
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2026-01-22 10:39:29 -0500

    restart again dont trust on ai

--- controllers/auth_controller.py
+++
@@ -1,7 +0,0 @@
-from models.user import User
-
-def verify_user(username, password):
-    user = User.query.filter_by(username=username).first()
-    if user and user.check_password(password):
-        return user
-    return None
--- models/audit.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime, timezone
-from models import db
-
-class AuditMixin(object):
-    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
-    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc), nullable=False)
